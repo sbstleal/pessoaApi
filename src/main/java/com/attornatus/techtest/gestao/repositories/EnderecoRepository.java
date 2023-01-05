8bfa4e21c3d05beb5fc52f1e02f70a2842723537
Sebastião Leal <sebastiaodeoleal@gmail.com>
2023-01-05 20:42:31 -0300

    criado os repositories

--- src/main/java/com/attornatus/techtest/gestao/repositories/EnderecoRepository.java
+++ src/main/java/com/attornatus/techtest/gestao/repositories/EnderecoRepository.java
@@ -1,9 +1,9 @@
 package com.attornatus.techtest.gestao.repositories;
 
-import com.attornatus.techtest.gestao.domain.models.Pessoa;
+import com.attornatus.techtest.gestao.domain.models.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
 }
